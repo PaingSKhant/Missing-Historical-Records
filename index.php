7dad10f138d33aefd5f07c91d8578408c20b8fbd
Paing Soe Khant <babyboss7916@gmail.com>
2024-04-08 12:10:55 +0630

    wrote UI for book list page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once "./controller/controller.php";
 
@@ -11,21 +11,44 @@ $bookData = $historicalBook->jsonConnect();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book-Library</title>
 </head>
+
 <body>
-<div class="">
-    <!-- navigation bar  -->
-    <div class="container mx-auto p-4   ">
-        <i></i><h1 class="text-2xl font-bold">Book Library</h1>
-        <hr>
-      
+    <div class="font-mono">
+        <!-- navigation bar  -->
+        <div class="container mx-auto p-4   ">
+            <h1 class="text-2xl font-bold">Book Library</h1>
+            <hr>
+            <!-- book list card  -->
+            <div class="flex flex-wrap justify-around mt-4">
+
+<?php foreach ($bookData as $book) : ?>
+
+    <div class="w-96  mt-4 m-5  p-4 ">
+
+        <img class=" w-96 h-96 rounded-xl" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/history-book-cover-design-template-0e3961aae83cdeab2d3b120dd2d7063c_screen.jpg?ts=1692216756" alt="recipeimage">
+
+        <div class="mt-3  font-mono">
+
+            <h1 class=" text-black text-xl mt-4"><?php echo $book['BookTitle'] ?></h1>
+
+        </div>
+
     </div>
-    
+
+<?php endforeach; ?>
+
 </div>
+
+        </div>
+
+    </div>
 </body>
 <script src="https://cdn.tailwindcss.com"></script>
+
 </html>
